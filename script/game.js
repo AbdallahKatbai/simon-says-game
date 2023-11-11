8cf4a933729f4950988248c7d348505c24ed6c7c
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-11-11 22:37:29 +0200

    added the level-genration background fn

--- script/game.js
+++ script/game.js
@@ -1,11 +1,30 @@
 console.log("game didn't start yet")
 
-const lvl_title = document.getElementsByTagName('body')[0]
-lvl_title.addEventListener('keypress', start_game)
+const body = document.getElementsByTagName('body')[0]
+const lvl_title = document.getElementById ('level-title')
+let container = document.getElementsByClassName('container')[0]
+const colorcode = {
+    0: 'green',
+    1: 'red',
+    2: 'yellow',
+    3: 'blue'
+}
+let lvl_order = []
+
+body.addEventListener('keypress', start_game)
 
 function start_game(){
     console.log('game started')
-    lvl_title.removeEventListener('keypress', start_game)
+    //body.removeEventListener('keypress', start_game)
+    //lvl_title.parentNode.removeChild(lvl_title)
+    add_level()
+}
+
+function add_level(){
+    const n = Math.floor(Math.random()*4)
+    console.log(n)
+    lvl_order.push(colorcode[n])
+    console.log(lvl_order)
 }
 
 
