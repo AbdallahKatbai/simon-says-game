0c527bfd78ac659d998ee3b7857713c28ca49acf
Abdallah-Katbai <abdallah.katbai@gmail.com>
2023-11-11 21:43:24 +0200

    adjusted the start_game fn to respond once only

--- script/game.js
+++ script/game.js
@@ -1,7 +1,11 @@
 console.log("game didn't start yet")
+
 const lvl_title = document.getElementsByTagName('body')[0]
 lvl_title.addEventListener('keypress', start_game)
 
 function start_game(){
-console.log('game started')
+    console.log('game started')
+    lvl_title.removeEventListener('keypress', start_game)
 }
+
+
